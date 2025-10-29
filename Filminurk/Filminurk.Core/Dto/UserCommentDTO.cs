2853856e22ad1cf22918808778a6ca242c1fb252
SavvaS250 <smirnyaginsavva@gmail.com>
2025-10-29 11:42:03 +0200

    finished movies started usercomments

---
+++ Filminurk/Filminurk.Core/Dto/UserCommentDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Dto
+{
+    public class UserCommentDTO
+    {
+        [Key]
+        public Guid? CommentID { get; set; }
+        public string? CommenterUserID { get; set; }
+        public string CommentBody { get; set; }
+        public int CommetredScore { get; set; }
+        public int IsHelpful { get; set; }//👍
+        public int IsHarmful { get; set; }//👎
+
+        /*Andmebaasi jaoks vajalikud andmed*/
+        public DateTime CommentCreatedAt { get; set; }
+        public DateTime CommentModified { get; set; }
+        public DateTime? CommentDeleteAt { get; set; }
+    }
+}
---
+++ Filminurk/Filminurk/Models/UserComments/UserCommentsIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.UserComments
+{
+    public class UserCommentsIndexViewModel
+    {
+        [Key]
+        public Guid? CommentID { get; set; }
+        public string? CommenterUserID { get; set; }
+        public string CommentBody { get; set; }
+        public int? CommetredScore { get; set; }
+        public int? IsHelpful { get; set; }//👍
+        public int IsHarmful { get; set; }//👎
+
+        /*Andmebaasi jaoks vajalikud andmed*/
+        public DateTime CommentCreatedAt { get; set; }
+        public DateTime? CommentModified { get; set; }
+        public DateTime? CommentDeleteAt { get; set; }
+    }
+}
