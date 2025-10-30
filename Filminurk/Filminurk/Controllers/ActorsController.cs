749cbeaac27c6c69025af80cef6e7c09968a90b8
SavvaS250 <smirnyaginsavva@gmail.com>
2025-10-30 09:38:05 +0200

    Added ActrosController and added actor to Context

---
+++ Filminurk/Filminurk/Controllers/ActorsController.cs
@@ -0,0 +1,29 @@
+﻿using Filminurk.Data;
+using Filminurk.Models.Actros;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly FilminurkTARpe24Context _context;
+
+        public ActorsController(FilminurkTARpe24Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Actors.Select(x => new ActorsIndexViewModel
+            {
+                ActorID = x.ActorID,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                NickName = x.NickName,
+                ActorType = x.ActorType,
+                CareerStart = x.CareerStart,
+            });
+            return View(result);
+        }
+    }
+}
