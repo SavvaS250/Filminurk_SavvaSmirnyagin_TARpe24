605f279a420ef5da28bf39e3cf28f2822b07560e
SavvaS250 <smirnyaginsavva@gmail.com>
2025-10-30 09:15:42 +0200

    Added ActorDTO and also added EtryCreatedAt and EntryModifiedAt to dto and domain.

--- Filminurk/Filminurk.Core/Dto/ActorDTO.cs
+++ Filminurk/Filminurk.Core/Dto/ActorDTO.cs
@@ -4,28 +4,28 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Filminurk.Core.Domain;
 
-namespace Filminurk.Core.Domain
+namespace Filminurk.Core.Dto
 {
-    public class Actor
+    public class ActorDTO
     {
         [Key]
-        public Guid ActorID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public Guid? ActorID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string? NickName { get; set; }
-        List<string>? MoviesActedFor {  get; set; }
+        List<string>? MoviesActedFor { get; set; }
         public Guid? PortraitID { get; set; }
 
         // neli minu oma
-        public int ActorAge { get; set; }
+        public int? ActorAge { get; set; }
         public ActorType? ActorType { get; set; }
-        public DateTime CareerStart {  get; set; }
-        public DateTime? CareerEnd {  get; set; }
+        public DateTime? CareerStart { get; set; }
+        public DateTime? CareerEnd { get; set; }
 
         /* andmebaasi jaoks vajalikud */
         public DateTime? EntryCreatedAt { get; set; }
         public DateTime? EntryModifiedAt { get; set; }
-
     }
 }
