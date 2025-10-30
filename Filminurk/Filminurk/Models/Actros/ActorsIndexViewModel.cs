4d94c6c8eadc1ea2d5aa8116de3c488fc0342377
SavvaS250 <smirnyaginsavva@gmail.com>
2025-10-30 09:31:00 +0200

    Added Actor index view but its empty and added ActorIndexViewModel

---
+++ Filminurk/Filminurk/Models/Actros/ActorsIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Filminurk.Core.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.Actros
+{
+    public class ActorsIndexViewModel
+    {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? NickName { get; set; }
+
+        public ActorType? ActorType { get; set; }
+        public DateTime? CareerStart { get; set; }
+        
+    }
+}
