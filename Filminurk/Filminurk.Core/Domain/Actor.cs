92b0d947086d7e9377db05df6e3dc208d0bffdb0
SavvaS250 <smirnyaginsavva@gmail.com>
2025-10-30 09:00:18 +0200

    Actor Domain is done!

---
+++ Filminurk/Filminurk.Core/Domain/Actor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public class Actor
+    {
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? NickName { get; set; }
+        List<string>? MoviesActedFor {  get; set; }
+        public Guid? PortraitID { get; set; }
+
+        // kolm minu oma
+        public int ActorAge { get; set; }
+        public ActorType? ActorType { get; set; }
+        public DateTime CareerStart {  get; set; }
+        public DateTime? CareerEnd {  get; set; }
+
+    }
+}
