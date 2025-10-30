2aa167c2adc2ac57d1d5a77723476e381f2169f3
SavvaS250 <smirnyaginsavva@gmail.com>
2025-10-30 10:19:24 +0200

    Added create to actorsController

--- Filminurk/Filminurk/Controllers/ActorsController.cs
+++ Filminurk/Filminurk/Controllers/ActorsController.cs
@@ -1,4 +1,6 @@
-﻿using Filminurk.Data;
+﻿using Filminurk.Core.Dto;
+using Filminurk.Data;
+using Filminurk.Models.Actors;
 using Filminurk.Models.Actros;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,6 +26,44 @@ namespace Filminurk.Controllers
                 CareerStart = x.CareerStart,
             });
             return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ActorsCreateUpdateViewModel result = new();
+            return View("CreateUpdate" ,result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ActorsCreateUpdateViewModel vm)
+        {
+            if (ModelState.IsValid == true)
+            {
+
+                var dto = new ActorDTO()
+                {
+                    ActorID = vm.ActorID,
+                    FirstName = vm.FirstName,
+                    LastName = vm.LastName,
+                    NickName = vm.NickName,
+                    //MoviesActedFor = vm.MoviesActedFor,
+                    //PortraitID = vm.PortraitID,
+                    ActorAge = vm.ActorAge,
+                    ActorType = vm.ActorType,
+                    CareerStart = vm.CareerStart,
+                    CareerEnd = vm.CareerEnd,
+                    EntryCreatedAt = vm.EntryCreatedAt,
+                    EntryModifiedAt = vm.EntryModifiedAt,
+                };
+                var result = await _actorServices.Create(dto);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Filminurk/Filminurk/Models/Actors/ActorsCreateUpdateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Filminurk.Core.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.Actors
+{
+    public class ActorsCreateUpdateViewModel
+    {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? NickName { get; set; }
+        List<string>? MoviesActedFor { get; set; }
+        public Guid? PortraitID { get; set; }
+
+        // neli minu oma
+        public int ActorAge { get; set; }
+        public ActorType? ActorType { get; set; }
+        public DateTime CareerStart { get; set; }
+        public DateTime? CareerEnd { get; set; }
+
+        /* andmebaasi jaoks vajalikud */
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
