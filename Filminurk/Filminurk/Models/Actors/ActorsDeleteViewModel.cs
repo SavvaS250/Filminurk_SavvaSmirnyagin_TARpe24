b8331993b321f5ea2bafcee8bd9eb1902df996a6
SavvaS250 <smirnyaginsavva@gmail.com>
2025-10-31 12:29:55 +0200

    Delete bug fixed

--- Filminurk/Filminurk/Models/Actors/ActorsDeleteViewModel.cs
+++ Filminurk/Filminurk/Models/Actors/ActorsDeleteViewModel.cs
@@ -7,16 +7,16 @@ namespace Filminurk.Models.Actors
     {
         [Key]
         public Guid? ActorID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string? NickName { get; set; }
         List<string>? MoviesActedFor { get; set; }
         public Guid? PortraitID { get; set; }
 
         // neli minu oma
-        public int ActorAge { get; set; }
+        public int? ActorAge { get; set; }
         public ActorType? ActorType { get; set; }
-        public DateTime CareerStart { get; set; }
+        public DateTime? CareerStart { get; set; }
         public DateTime? CareerEnd { get; set; }
 
         /* andmebaasi jaoks vajalikud */
