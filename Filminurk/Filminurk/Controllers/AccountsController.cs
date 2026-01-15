09dcdf9f7607ed78a9cab5df39bf8a873192ce34
SavvaS250 <smirnyaginsavva@gmail.com>
2026-01-15 15:19:23 +0200

    Now it works

--- Filminurk/Filminurk/Controllers/AccountsController.cs
+++ Filminurk/Filminurk/Controllers/AccountsController.cs
@@ -254,7 +254,7 @@ namespace Filminurk.Controllers
             var result = await _userManager.ConfirmEmailAsync(user, token);
             if (result.Succeeded)
             {
-                return View("Login");
+                return View("ConfirmEmail");
             }
             return BadRequest();
         }
